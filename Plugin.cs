e7ef6b2c34efec281cd254ad69da94fd0812aa82
SpeedxPz <admin@4th.in.th>
2026-07-30 14:24:51 +0700

    Gate launcher tile to World phase

--- Plugin.cs
+++ Plugin.cs
@@ -63,7 +63,8 @@ public sealed class Plugin : IStellarPlugin
             IconPng: LoadIconPng(),
             IconKey: null,
             OnOpen:  () => _window.SetVisible(true))
-        { Group = LauncherGroup.Plugin });
+        { Group = LauncherGroup.Plugin,
+          ShouldShow = () => _services.ClientState.Phase == GamePhase.World });
 
         _services.Log.Info("[MinimalNameplate] constructed");
     }
